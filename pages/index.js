46cd21f81b0617ee53daf65683622939c8cfa522
postor <postor@gmail.com>
2017-07-04 23:01:28 +0800

    v1

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-import {fetchJSON} from '../tools/fetch'
-import Dashboard from '../components/templates/Dashboard.js'
 
+import { connect } from 'react-redux'
+import Dashboard from '../components/templates/Dashboard.js'
+import wrapper from '../tools/wrapper'
+import { getContextedFetch } from '../tools/fetch'
+import { fetchJSON, getPath } from '../tools/store/json'
+
+const storePath = 'app_pages_index'
 
 const Index = (props) => (
   <Dashboard {...props} />
 )
 
-Index.getInitialProps = async ({ req, res }) => {
-  return await fetchJSON('/static/pages/index.json', {}, req, res)
+Index.getInitialProps = async (ctx) => {
+  return await ctx.store.dispatch(fetchJSON('/static/pages/index.json', storePath, getContextedFetch(ctx), ctx.store))
+    .catch((e) => {
+      console.log(e)
+    })
 }
 
-export default Index
+export default wrapper(connect(state => state[storePath])(Index))
--- tools/fetch.js
+++ tools/fetch.js
@@ -1,13 +1,34 @@
 import getFetch from 'next-fetch'
-const fetch = getFetch()
+const cookieFetch = getFetch()
 
-export default fetch
-
-export const fetchJSON = async (url,options,req,res)=>{
-  var r = await fetch(apiUrl(url,req),options,req,res)
-  return r.json()
+/**
+ * 加载本地资源
+ * 
+ * @export
+ * @param {any} url 
+ * @param {any} option 
+ * @param {any} req 
+ * @param {any} res 
+ * @returns 
+ */
+export default function fetch(url,option,req,res){
+  return cookieFetch(apiUrls(url, req), option, req, res)
 }
 
-function apiUrl(path, req) {
+/**
+ * 获取绑定的fetch
+ * 
+ * @export
+ * @param {any} { req, res } 
+ * @returns 
+ */
+export function getContextedFetch({ req, res }) {
+  return ((req, res, url, option) => {
+    console.log(['req:'+typeof req,'window:'+typeof window])
+    return fetch(url, option, req, res)
+  }).bind(null, req, res)
+}
+
+export function apiUrls(path, req) {
   return req ? req.protocol + '://' + req.get('host') + path : path
 }
